3188dbbbac0e67ddb7a23a078023675308bacc29
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2025-12-27 18:40:40 +0000

    commit todays

---
+++ Basics/Datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+let age=23;//number
+let name='pratik'//string
+let loggedIn=true//boolean
+console.log(typeof loggedIn);
+let year=null //null is standalone Value in js
+let date 
+console.log(typeof year)  //object
+console.log(date)
+console.log(typeof date)
+//typeof -> tell's us type of value 
---
+++ Basics/Variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "pratikfulshete@gmail.com"
+var accountPassword = "1234"
+accountCity= "Pune"
+ //accountId =2
+ console.log(accountId);
+ accountEmail="pf@gmail.com";
+ accountPassword="000000";
+ accountCity="Mumbai";
+ console.table([accountEmail,accountPassword,accountCity])
+ 
