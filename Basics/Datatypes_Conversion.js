22359f728f0164d843fb3b3865b967269758e8c1
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2025-12-28 23:34:10 +0530

    commit

---
+++ Basics/Datatypes_Conversion.js
@@ -0,0 +1,8 @@
+let score = "33"
+console.log( typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+let score2="33xyz"
+let valueInNumber2=Number(score2)
+console.log(valueInNumber2)
+console.log( typeof valueInNumber2)
